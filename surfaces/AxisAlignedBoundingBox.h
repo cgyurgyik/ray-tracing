593da7880d70cbfa7c01384fff3923eed0f645eb
Chris Gyurgyik <gyurgyikcp@gmail.com>
2020-01-04 11:31:19 -0500

    Add Andrew Kensler AABB:hit() method.

--- surfaces/AxisAlignedBoundingBox.h
+++ surfaces/AxisAlignedBoundingBox.h
@@ -26,37 +26,18 @@ public:
         return AxisAlignedBoundingBox(small, large);
     }
 
-    // TODO: Update using Andrew Kensler's faster method which incorporates std::swap().
     bool hit(const Ray& ray, value_type t_min, value_type t_max) const {
-        const value_type inv_x_direction  = 1.0 / ray.direction().x();
-        const value_type x_min = get_min(min_.x() - ray.origin().x() * inv_x_direction,
-                                         max_.x() - ray.origin().x() * inv_x_direction);
-        const value_type x_max = get_max(min_.x() - ray.origin().x() / ray.direction().x(),
-                                         max_.x() - ray.origin().x() / ray.direction().x());
-
-        t_min = get_max(x_min, t_min);
-        t_max = get_min(x_max, t_max);
-        if (t_max <= t_min) return false;
-
-        const value_type inv_y_direction  = 1.0 / ray.direction().y();
-        const value_type y_min = get_min(min_.y() - ray.origin().y() * inv_y_direction,
-                                         max_.y() - ray.origin().y() * inv_y_direction);
-        const value_type y_max = get_max(min_.y() - ray.origin().y() * inv_y_direction,
-                                         max_.y() - ray.origin().y() * inv_y_direction);
-
-        t_min = get_max(y_min, t_min);
-        t_max = get_min(y_max, t_max);
-        if (t_max <= t_min) return false;
-
-        const value_type inv_z_direction  = 1.0 / ray.direction().z();
-        const value_type z_min = get_min(min_.z() - ray.origin().z() * inv_z_direction,
-                                         max_.z() - ray.origin().z() * inv_z_direction);
-        const value_type z_max = get_max(min_.z() - ray.origin().z() * inv_z_direction,
-                                         max_.z() - ray.origin().z() * inv_z_direction);
-
-        t_min = get_max(z_min, t_min);
-        t_max = get_min(z_max, t_max);
-        return (t_max <= t_min);
+        for (int a = 0; a < 3; ++a) {
+            const value_type invD = 1.0 / ray.direction().to_free()[a];
+            value_type t0 = (min()[a] - ray.origin()[a]) * invD;
+            value_type t1 = (max()[a] - ray.origin()[a]) * invD;
+            if (invD < 0.0) std::swap(t0, t1);
+            t_min = t0 > t_min ? t0 : t_min;
+            t_max = t1 < t_max ? t1 : t_max;
+            if (t_max <= t_min)
+                return false;
+        }
+        return true;
     }
 
 private:
